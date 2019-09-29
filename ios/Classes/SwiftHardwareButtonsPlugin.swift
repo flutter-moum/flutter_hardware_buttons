0b843720b2cc03c0f646d14f2e301f35c4835739
eojin <eozinee@gmail.com>
2019-09-29 22:51:09 +0900

    Add iOS Native to connect .dart

--- ios/Classes/SwiftHardwareButtonsPlugin.swift
+++ ios/Classes/SwiftHardwareButtonsPlugin.swift
@@ -1,77 +1,17 @@
 import Flutter
 import UIKit
-import AVFoundation
 
 public class SwiftHardwareButtonsPlugin: NSObject, FlutterPlugin {
-
-    private var volumeLevel: Float = 0.0
     
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let volumeChannel = FlutterMethodChannel(name: "hardware_buttons",
+        let channel = FlutterMethodChannel(name: "hardware_buttons",
                                                  binaryMessenger: registrar.messenger())
         let instance = SwiftHardwareButtonsPlugin()
-        registrar.addMethodCallDelegate(instance, channel: volumeChannel)
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
 
     public func handle(_ call: FlutterMethodCall,
                        result: @escaping FlutterResult) {
-        switch call.method {
-        case "getPlatformVersion":
-            result("iOS " + UIDevice.current.systemVersion)
-            break
-        default:
-            result(FlutterMethodNotImplemented)
-        }
+        result("iOS " + UIDevice.current.systemVersion)
     }
-    
-    override init() {
-        super.init()
-        volumeChangeDetect()
-    }
-    
-    override public func observeValue(forKeyPath keyPath: String?,
-                                      of object: Any?,
-                                      change: [NSKeyValueChangeKey : Any]?,
-                                      context: UnsafeMutableRawPointer?) {
-        if keyPath == "outputVolume"{
-            let audioSession = AVAudioSession.sharedInstance()
-            if audioSession.outputVolume > volumeLevel {
-                showSimpleAlert(title: "Volume Up Button",
-                                message: String(volumeLevel))
-            }
-            if audioSession.outputVolume < volumeLevel {
-                showSimpleAlert(title: "Volume Down Button",
-                                message: String(volumeLevel))
-            }
-            volumeLevel = audioSession.outputVolume
-        }
-    }
-    
-    private func volumeChangeDetect() {
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setActive(true)
-            audioSession.addObserver(self,
-                                     forKeyPath: "outputVolume",
-                                     options: .new,
-                                     context: nil)
-            volumeLevel = audioSession.outputVolume
-        } catch {
-            print("error")
-        }
-    }
-    
-    private func showSimpleAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title,
-                                      message: message,
-                                      preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK",
-                                      style: UIAlertActionStyle.default,
-                                      handler: nil))
-        UIApplication.shared
-            .keyWindow?.rootViewController?.present(alert,
-                                                    animated: true,
-                                                    completion: nil)
-    }
-
 }
