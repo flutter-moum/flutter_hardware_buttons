9d236b425702b81b90bc059f2adc7cb685ef1f2f
kevin-ee <kevin.ee@kakaocorp.com>
2019-10-06 17:41:19 +0900

    Run flutte format

--- lib/hardware_buttons.dart
+++ lib/hardware_buttons.dart
@@ -5,15 +5,17 @@ import 'package:flutter/services.dart';
 const _VOLUME_BUTTON_CHANNEL_NAME = 'flutter.moum.hardware_buttons.volume';
 const _HOME_BUTTON_CHANNEL_NAME = 'flutter.moum.hardware_buttons.home';
 
-const EventChannel _volumeButtonEventChannel = EventChannel(_VOLUME_BUTTON_CHANNEL_NAME);
-const EventChannel _homeButtonEventChannel = EventChannel(_HOME_BUTTON_CHANNEL_NAME);
+const EventChannel _volumeButtonEventChannel =
+    EventChannel(_VOLUME_BUTTON_CHANNEL_NAME);
+const EventChannel _homeButtonEventChannel =
+    EventChannel(_HOME_BUTTON_CHANNEL_NAME);
 
 Stream<VolumeButtonEvent> _volumeButtonEvents;
 Stream<VolumeButtonEvent> get volumeButtonEvents {
   if (_volumeButtonEvents == null) {
     _volumeButtonEvents = _volumeButtonEventChannel
-      .receiveBroadcastStream()
-      .map((dynamic event) => _eventToVolumeButtonEvent(event));
+        .receiveBroadcastStream()
+        .map((dynamic event) => _eventToVolumeButtonEvent(event));
   }
   return _volumeButtonEvents;
 }
@@ -22,8 +24,8 @@ Stream<HomeButtonEvent> _homeButtonEvents;
 Stream<HomeButtonEvent> get homeButtonEvents {
   if (_homeButtonEvents == null) {
     _homeButtonEvents = _homeButtonEventChannel
-      .receiveBroadcastStream()
-      .map((dynamic event) => HomeButtonEvent.INSTANCE);
+        .receiveBroadcastStream()
+        .map((dynamic event) => HomeButtonEvent.INSTANCE);
   }
   return _homeButtonEvents;
 }
