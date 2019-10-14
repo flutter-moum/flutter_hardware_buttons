ad9a335752d9d24b38ff92057a0b885e2e5de425
eojin <eozinee@gmail.com>
2019-10-14 17:11:25 +0900

    [iOS] Fix Home And Lock Button Detection

--- ios/Classes/HomeButtonStreamHandler.swift
+++ ios/Classes/HomeButtonStreamHandler.swift
@@ -11,17 +11,20 @@ public class HomeButtonStreamHandler: NSObject, FlutterStreamHandler {
     
     private var eventSink: FlutterEventSink?
     private let notificationCenter = NotificationCenter.default
+    private var homeTask: DispatchWorkItem?
     
     public func onListen(withArguments arguments: Any?,
                          eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         self.eventSink = events
         registerHomeObserver()
+        registerLockObserver()
         return nil
     }
     
     public func onCancel(withArguments arguments: Any?) -> FlutterError? {
         eventSink = nil
         removeHomeObserver()
+        removeLockObserver()
         return nil
     }
     
@@ -34,12 +37,60 @@ public class HomeButtonStreamHandler: NSObject, FlutterStreamHandler {
             object: nil)
     }
     
+    // Register Lock Notification
+    private func registerLockObserver() {
+        CFNotificationCenterAddObserver(
+            CFNotificationCenterGetDarwinNotifyCenter(),
+            Unmanaged.passUnretained(self).toOpaque(),
+            displayStatusChangedCallback,
+            "com.apple.springboard.lockcomplete" as CFString,
+            nil,
+            .deliverImmediately)
+    }
+    
     // Remove Home Notification
     private func removeHomeObserver() {
         notificationCenter.removeObserver(Notification.Name.UIApplicationWillResignActive)
     }
+
+    // Remove Lock Notification
+    private func removeLockObserver() {
+        CFNotificationCenterRemoveObserver(
+            CFNotificationCenterGetLocalCenter(),
+            Unmanaged.passUnretained(self).toOpaque(),
+            nil,
+            nil)
+    }
     
+    // Home Button Detection
     @objc func applicationWillResignActive(){
-        eventSink?(0)
+        homeTask = DispatchWorkItem {
+            print("HOME")
+        }
+        DispatchQueue.main.asyncAfter(
+            deadline: DispatchTime.now() + 0.2,
+            execute: homeTask ?? DispatchWorkItem(block: {
+                print("error")
+            }))
+    }
+    
+    // Lock Button Detection
+    private func displayStatusChanged(_ lockState: String) {
+        if lockState == "com.apple.springboard.lockcomplete" {
+            print("LOCK")
+            homeTask?.cancel()
+//            eventSink?(0)
+        }
+    }
+    
+    private let displayStatusChangedCallback: CFNotificationCallback = { _, cfObserver, cfName, _, _ in
+        guard let lockState = cfName?.rawValue as String? else {
+            return
+        }
+        
+        let catcher = Unmanaged<HomeButtonStreamHandler>
+            .fromOpaque(UnsafeRawPointer(OpaquePointer(cfObserver)!))
+            .takeUnretainedValue()
+        catcher.displayStatusChanged(lockState)
     }
 }
--- ios/Classes/LockButtonStreamHandler.swift
+++ ios/Classes/LockButtonStreamHandler.swift
@@ -15,47 +15,47 @@ public class LockButtonStreamHandler: NSObject, FlutterStreamHandler {
     public func onListen(withArguments arguments: Any?,
                          eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         self.eventSink = events
-        registerLockObserver()
+//        registerLockObserver()
         return nil
     }
     
     public func onCancel(withArguments arguments: Any?) -> FlutterError? {
         eventSink = nil
-        removeLockObserver()
+//        removeLockObserver()
         return nil
     }
     
-    // Register Lock Notification
-    private func registerLockObserver() {
-        CFNotificationCenterAddObserver(
-            CFNotificationCenterGetDarwinNotifyCenter(),
-            Unmanaged.passUnretained(self).toOpaque(),
-            displayStatusChangedCallback,
-            "com.apple.springboard.lockstate" as CFString,
-            nil,
-            .deliverImmediately)
-    }
-    
-    // Remove Lock Notification
-    private func removeLockObserver() {
-        CFNotificationCenterRemoveObserver(CFNotificationCenterGetLocalCenter(),
-                                           Unmanaged.passUnretained(self).toOpaque(),
-                                           nil,
-                                           nil)
-    }
-    
-    private func displayStatusChanged(_ lockState: String) {
-        if lockState == "com.apple.springboard.lockstate" {
-            eventSink?(0)
-        }
-    }
-    
-    private let displayStatusChangedCallback: CFNotificationCallback = { _, cfObserver, cfName, _, _ in
-        guard let lockState = cfName?.rawValue as String? else {
-            return
-        }
-        
-        let catcher = Unmanaged<LockButtonStreamHandler>.fromOpaque(UnsafeRawPointer(OpaquePointer(cfObserver)!)).takeUnretainedValue()
-        catcher.displayStatusChanged(lockState)
-    }
+//    // Register Lock Notification
+//    private func registerLockObserver() {
+//        CFNotificationCenterAddObserver(
+//            CFNotificationCenterGetDarwinNotifyCenter(),
+//            Unmanaged.passUnretained(self).toOpaque(),
+//            displayStatusChangedCallback,
+//            "com.apple.springboard.lockstate" as CFString,
+//            nil,
+//            .deliverImmediately)
+//    }
+//    
+//    // Remove Lock Notification
+//    private func removeLockObserver() {
+//        CFNotificationCenterRemoveObserver(CFNotificationCenterGetLocalCenter(),
+//                                           Unmanaged.passUnretained(self).toOpaque(),
+//                                           nil,
+//                                           nil)
+//    }
+//    
+//    private func displayStatusChanged(_ lockState: String) {
+//        if lockState == "com.apple.springboard.lockstate" {
+//            eventSink?(0)
+//        }
+//    }
+//    
+//    private let displayStatusChangedCallback: CFNotificationCallback = { _, cfObserver, cfName, _, _ in
+//        guard let lockState = cfName?.rawValue as String? else {
+//            return
+//        }
+//        
+//        let catcher = Unmanaged<LockButtonStreamHandler>.fromOpaque(UnsafeRawPointer(OpaquePointer(cfObserver)!)).takeUnretainedValue()
+//        catcher.displayStatusChanged(lockState)
+//    }
 }
